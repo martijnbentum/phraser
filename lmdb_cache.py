d462982010a94b0bfbcadf1a11948c9a4fbde159
martijnbentum <bentummartijn@gmail.com>
2025-12-02 18:01:00 +0100

    updated loading to be faster, both the init of new objects (with __new__ and dict update) and loading from ldmb (with temporary disabling of garbage collection) also not using keys.index in a for loop helped a lot. loading 360K items from 500s -> 5s

--- lmdb_cache.py
+++ lmdb_cache.py
@@ -1,7 +1,9 @@
+import gc
 import lmdb_helper
 import lmdb_key
 import locations
 import pickle
+import time
 
 
 class Cache:
@@ -12,14 +14,14 @@ class Cache:
     - resolver for parent, children, speaker, audio
     """
 
-    def __init__(self, env = None, path = locations.cgn_lmdb, vebose = False ):
+    def __init__(self, env = None, path = locations.cgn_lmdb, verbose= False ):
         self.env = lmdb_helper.open_lmdb(env, path)
         self._cache = {}      # key:str → object
         self.CLASS_MAP = {}   # filled externally (Audio, etc.)
         self.save_counter = {}
         self.load_counter = {}
         self.save_key_counter = {}
-        self.vebose = vebose
+        self.verbose = verbose
 
     def register(self, cls):
         self.CLASS_MAP[cls.__name__] = cls
@@ -53,31 +55,52 @@ class Cache:
             if self.verbose: print(' Found in cache.')
             return self._cache[key]
         if self.verbose: print(' Not in cache. Loading from LMDB...')
+        
         object_type = lmdb_key.key_to_object_type(key)
         cls = self.CLASS_MAP[object_type]
-        obj = cls(key = key)
+        data = lmdb_helper.load(key = key, env = self.env)
+        obj = data_dict_to_instance(cls, data)
         self._cache[key] = obj
         self.load_counter[object_type] += 1
         return obj
 
     def load_many(self, keys, with_links = False):
-        objs = []
+        if len(keys) == 0: return []
+        if len(keys) == 1: return [self.load(keys[0], with_links)]
+        start = time.time()
+        objs = [ [] for _ in range(len(keys)) ]
+        key_to_index = {key: i for i, key in enumerate(keys)}
+        print(time.time() - start, 'objs initialized')
         found_in_cache = []
         not_found_in_cache = []
-        for key in keys:
+        for index, key in enumerate(keys):
             if key in self._cache:
                 found_in_cache.append(key)
-                objs.append(self._cache[key])
+                objs[index] = self._cache[key]
             else:
                 not_found_in_cache.append(key)
+        print(time.time() - start, 'cache checked')
         if len(not_found_in_cache) == 0: return objs
         if len(not_found_in_cache) == 1: 
-            objs.append(self.load(not_found_in_cache[0], with_links))
-        results = lmdb_helper.read_many(keys = not_found_in_cache, 
-            env = self.env)
-        self._cache.update(results)
-        objs += [results[k] for k in not_found_in_cache]
-        objs = sorted(objs, key = lambda x: keys.index(x.key))
+            index = key_to_index[not_found_in_cache[0]]
+            objs[index] = self.load(not_found_in_cache[0], with_links)
+        if len(not_found_in_cache) > 100_000: gc.disable()
+        try:
+            results = lmdb_helper.load_many(keys = not_found_in_cache, 
+                env = self.env)
+            print(time.time() - start, 'lmdb data loaded')
+            for key, data in zip(not_found_in_cache, results):
+                index = key_to_index[key]
+                object_type = lmdb_key.key_to_object_type(key)
+                cls = self.CLASS_MAP[object_type]
+                obj = data_dict_to_instance(cls, data)
+                self._cache[key] = obj
+                objs[index] = obj
+                self.load_counter[object_type] += 1
+            print(time.time() - start, 'objs created')
+        finally:
+            if len(not_found_in_cache) > 100_000: gc.enable()
+        print('gc enabled', time.time() - start)
         return objs
 
     def delete(self, key):
@@ -98,7 +121,34 @@ class Cache:
         self._object_type_to_keys_dict = d
         return self._object_type_to_keys_dict
 
+
+def data_dict_to_instance(cls, data):
+    obj = cls.__new__(cls)
+    data['object_type'] = cls.__name__
+    extra = data.pop('extra')
+    obj.__dict__.update(data)
+    obj.__dict__.update(extra)
+    return obj
     
 
+class Objects:
+    def __init__(self, cls, cache):
+        self.cls = cls
+        self.cache = cache
+        self.object_type = cls.__name__
 
+
+    def all(self, update = False):
+        d = self.cache.object_type_to_keys_dict(update = update)
+        m = f'No keys found for object type: {self.object_type}'
+        if self.object_type not in d:
+            raise ValueError(m)
+        selected_keys = d[self.object_type]
+        objs = self.cache.load_many(selected_keys)
+        return objs
+
+    def filter(self, filter_func, update = False):
+        all_objs = self.all(update = update)
+        selected_objs = [obj for obj in all_objs if filter_func(obj)]
+        return selected_objs
 
