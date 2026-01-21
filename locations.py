eaa7a0b94a09318810f1692e4466a749df1e5939
martijnbentum <bentummartijn@gmail.com>
2026-01-21 16:21:11 +0100

    added paths for textgrids

--- locations.py
+++ locations.py
@@ -4,3 +4,6 @@ data = Path('../data')
 data.mkdir(parents=True, exist_ok=True)
 default_lmdb = data / 'default_lmdb'
 cgn_lmdb = data / 'cgn_lmdb'
+
+textgrids = data / 'textgrids'
+cgn_ort_directory = data / 'ort'
