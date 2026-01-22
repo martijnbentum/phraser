9045a2f1c3ab923d71641f4a60b4d51e54d5d5d3
martijnbentum <bentummartijn@gmail.com>
2026-01-22 16:30:26 +0100

    added cgn locations

--- locations.py
+++ locations.py
@@ -5,5 +5,11 @@ data.mkdir(parents=True, exist_ok=True)
 default_lmdb = data / 'default_lmdb'
 cgn_lmdb = data / 'cgn_lmdb'
 
+audio_filenames = data / 'audio_filenames.txt'
+
 textgrids = data / 'textgrids'
 cgn_ort_directory = data / 'ort'
+
+cgn_base = Path('/vol/bigdata/corpora2/CGN2/')
+cgn_audio = cgn_base / 'data/audio/wav/'
+cgn_speaker_file = cgn_base / 'data/meta/text/speakers.txt'
