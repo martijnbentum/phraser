74fcd6f6e7ef000103d14b7ced287d4b7fc154e2
martijnbentum <bentummartijn@gmail.com>
2026-01-22 16:30:58 +0100

    added functions to load cgn data

--- process_cgn.py
+++ process_cgn.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from progressbar import progressbar
 from textgrid import TextGrid
 
-def textgrid_filenames(cgn_ort_directory = None):
+def ort_textgrid_filenames(cgn_ort_directory = None):
     if cgn_ort_directory is None:
         cgn_ort_directory = locations.cgn_ort_directory
     fn = list(cgn_ort_directory.glob('*.ort'))
@@ -15,6 +15,88 @@ def load_textgrid(filename):
     """Load a TextGrid file and return the TextGrid object."""
     tg = TextGrid.fromFile(filename)
     return tg
+
+    
+def load_speaker_file():
+    with locations.cgn_speaker_file.open('r', encoding='utf-8') as f:
+        lines = [x.split('\t') for x in f.read().split('\n') if x]
+    header = lines[0]
+    data = lines[1:]
+    return header, data
+
+def load_speaker_info(speaker_id, speaker_file = None):
+    if speaker_file: header, data = speaker_file
+    else: header, data = load_speaker_file()
+    for row in data:
+        if row[4] == speaker_id:
+            info = dict(zip(header, row))
+            return info
+    return None
+
+def load_cgn_audio_filenames():
+    with open(locations.audio_filenames, 'r', encoding='utf-8') as f:
+        fn = f.read().split('\n')
+    paths = [Path(x) for x in fn if x]
+    return paths
+
+def audio_filename_to_component(audio_filename):
+    return audio_filename.parent.parent.name
+
+def cgn_id_to_audio(cgn_id, cgn_audio_filenames = None):
+    if cgn_audio_filenames is None:
+        cgn_audio_filenames = load_cgn_audio_filenames()
+    for p in cgn_audio_filenames:
+        if p.stem == cgn_id:
+            return p
+
+def cgn_id_to_ort(cgn_id, cgn_ort_directory = None):
+    if cgn_ort_directory is None:
+        cgn_ort_directory = locations.cgn_ort_directory
+    p = cgn_ort_directory / f'{cgn_id}.ort'
+    if p.exists():
+        return p
+
+def textgrid_to_speaker_tiers(tg, exclude = ['BACKGROUND', 'COMMENT']):
+    names = [name for name in tg.getNames() if name not in exclude]
+    return names
+
+def handle_tier(tg, tier_name, audio_filename, component):
+    tier_index = tg.getNames().index(tier_name)
+    tier = tg.tiers[tier_index]
+    # speaker_info = load_speaker_info(tier_name, speakers_file)
+    # output = {'speaker': speaker_info, 'intervals': []}
+    output = []
+    for interval in tier.intervals:
+        start_time = interval.minTime
+        end_time = interval.maxTime
+        text = interval.mark
+        output_dir = locations.textgrids / tier_name
+        if not text: continue
+        d = {'start_time': start_time, 'end_time': end_time, 'text': text, 
+            'tier_name': tier_name, 'component': component, 
+            'audio_filename': str(audio_filename), 
+            'output_dir': str(output_dir)}
+        output.append(d)
+    return output
+
+
+def handle_cgn_id(cgn_id, cgn_audio_filenames = None, cgn_ort_directory = None):
+    if cgn_audio_filenames is None: 
+        cgn_audio_filenames = load_cgn_audio_filenames()
+    speakers_info = load_speaker_file()
+    audio_path = cgn_id_to_audio(cgn_id, cgn_audio_filenames)
+    ort_path = cgn_id_to_ort(cgn_id, cgn_ort_directory)
+    tg = load_textgrid(ort_path)
+    comp = audio_filename_to_component(audio_path)
+    output = []
+    tier_names = textgrid_to_speaker_tiers(tg)
+    for tier_name in tier_names:
+        tier_data = handle_tier(tg, tier_name, audio_path, comp)
+        output.extend(tier_data)
+    return output
+
+
+
 
 def make_or_load_ort_info_dict(cgn_ort_directory = None, overwrite=False):
     p = Path('../data/cgn_ort_info_dict.json')
