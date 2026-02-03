d9149f11c2a7f4bb1b4e1d3f308615b952498196
martijnbentum <bentummartijn@gmail.com>
2026-02-03 17:16:20 +0100

    added function to pretty print object dict

---
+++ utils.py
@@ -0,0 +1,38 @@
+R= "\033[91m"
+G= "\033[92m"
+B= "\033[94m"
+GR= "\033[90m"
+RE= "\033[0m"
+
+def pretty_print_object_dict(obj_dict):
+    '''pretty print dict with red keys and aligned outline.
+    returns a single formatted string.
+    '''
+    lines = []
+    width = max(len(str(k)) for k in obj_dict)
+
+    for key, value in obj_dict.items():
+        key_pad = ' ' * (width - len(str(key)))
+        prefix = f'{R}{key}{RE}{key_pad} : '
+
+        if isinstance(value, dict):
+            if not value:
+                lines.append(prefix + '{}')
+                continue
+            lines.append(prefix.rstrip())
+            sub_width = max(len(str(k)) for k in value)
+            for subkey, subval in value.items():
+                sub_pad = ' ' * (sub_width - len(str(subkey)))
+                lines.append(f'  {R}{subkey}{RE}{sub_pad} : {subval}')
+        elif isinstance(value, list):
+            if not value:
+                lines.append(prefix + '[]')
+                continue
+            lines.append(prefix.rstrip())
+            for item in value:
+                lines.append(f'  - {item}')
+        else:
+            lines.append(prefix + str(value))
+
+    return '\n'.join(lines)
+
