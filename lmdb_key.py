4d157c9bfc1fa2e18b49be427dc52fc3cf07a4bf
martijnbentum <bentummartijn@gmail.com>
2026-02-17 17:49:34 +0100

    rank class mapping via on dict; key to identifier now based on key only for speed up

--- lmdb_key.py
+++ lmdb_key.py
@@ -1,7 +1,7 @@
 import os
 import struct_key
 import struct_helper
-from struct_helper import CLASS_RANK_MAP
+from struct_helper import CLASS_RANK_MAP, RANK_CLASS_MAP
 
 def instance_to_child_time_scan_keys(instance):
     return struct_key.instance_to_child_time_scan_keys(instance)
@@ -23,7 +23,7 @@ def audio_id_segment_id_class_to_key(audio_id, segment_id, object_type,
     object_type: one of Audio, Phrase, Word, Syllable, Phone
     offset_ms: integer offset in milliseconds
     '''
-    rank = object_type_to_rank(object_type)
+    rank = CLASS_RANK_MAP[object_type]
     return struct_key.pack_segment_key(audio_id, rank, offset_ms, segment_id)
 
 def audio_id_to_key(audio_id):
@@ -44,47 +44,43 @@ def instance_to_rank(instance):
     Supports: Audio, Phrase, Word, Syllable, Phone.
     '''
     object_type = instance.__class__.__name__
-    return object_type_to_rank(object_type)
+    return CLASS_RANK_MAP[object_type]
 
-def object_type_to_rank(object_type):
-    '''Map an object type string to its rank.
-    '''
-    try:
-        return TYPE_TO_RANK_MAP[object_type]
-    except KeyError:
-        raise ValueError(f'Unsupported object type: {object_type}')
-
-
-def rank_to_object_type(rank):
-    '''Map a rank to its object type string.
-    '''
-    for k, v in TYPE_TO_RANK_MAP.items():
-        if v == rank:
-            return k
-    raise ValueError(f'Unsupported rank: {rank}')
 
 def key_to_object_type(key):
     '''Get object type string from LMDB key.
     '''
-    info = key_to_info(key)
-    return info['object_type']
+    if len(key) == struct_key.AUDIO_LEN: return 'Audio'
+    if len(key) == struct_key.SPEAKER_LEN: return 'Speaker'
+    rank = key[9]
+    return RANK_CLASS_MAP[rank]
 
 def key_to_identifier(key):
     '''Get identifier (UUID hex string) from LMDB key.
     '''
-    info = key_to_info(key)
-    return info['identifier']
+    if len(key) == struct_key.AUDIO_LEN: key[1:].hex()
+    if len(key) == struct_key.SPEAKER_LEN: return None
+    return key[-8:].hex()
     
 def key_to_audio_identifier(key):
-    info = key_to_info(key)
-    if info['object_type'] == 'Audio':
-        return info['identifier']
-    if info['object_type'] == 'Speaker':
-        return None
-    return info['audio_identifier']
+    if len(key) == struct_key.SPEAKER_LEN: return None
+    return key[1:9].hex()
 
 def audio_id_to_scan_prefix(audio_id, child_class = 'Phrase'):
     '''Get LMDB scan prefix for a given audio ID.
     '''
     return struct_key.pack_audio_prefix(audio_id, child_class)
+
+def speaker_audio_link(speaker, audio):
+    '''Make LMDB key for a speaker-audio pair.
+    speaker_id: hex string of speaker UUID
+    audio_id: hex string of audio UUID
+    '''
+    return struct_key.pack_speaker_audio_key(speaker.identifier, 
+        audio.identifier)
+
+def speaker_id_to_scan_prefix(speaker_id):
+    '''Get LMDB scan prefix for a given speaker ID.
+    '''
+    return struct_key.make_speaker_scan_prefix(speaker_id)
 
